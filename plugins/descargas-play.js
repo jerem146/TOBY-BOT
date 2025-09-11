e23742ff0a2de87389863e04ddf37cbb1afd61aa
Dioneibi-rip <fandubdreams@gmail.com>
2025-09-11 00:50:27 -0400

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -103,7 +103,7 @@ const handler = async (m, { conn, text, command }) => {
       await conn.sendMessage(m.chat, {
         video: buffer,
         fileName: `${videoData.title || "video"}.mp4`,
-        caption: `✧ 𝗧𝗶́𝘁𝘂𝗹𝗼 » ${title}`,
+        caption: `${title}`,
         mimetype: "video/mp4"
       }, { quoted: m })
 
