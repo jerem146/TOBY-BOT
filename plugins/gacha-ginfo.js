90f96683cd46b4b5a138e4e37550d5388365239d
Dioneibi-rip <fandubdreams@gmail.com>
2025-09-19 03:22:07 -0400

    Actualizar gacha-ginfo.js

--- plugins/gacha-ginfo.js
+++ plugins/gacha-ginfo.js
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 
 import { cooldowns as rwCooldowns } from './gacha-rollwaifu.js';
 import { cooldowns as claimCooldowns } from './gacha-claim.js';
-import { cooldowns as voteCooldowns, voteCooldownTime } from './vote.js';
+import { cooldowns as voteCooldowns, voteCooldownTime } from './gacha-vote.js';
 
 const charactersFilePath = './src/database/characters.json';
 
