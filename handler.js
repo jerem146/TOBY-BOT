fbb803149c2cdbc13c5b06bccc71986a741cb6d1
Dioneibi-rip <fandubdreams@gmail.com>
2025-09-21 09:16:03 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -8,30 +8,6 @@ import chalk from 'chalk'
 import failureHandler from './lib/respuesta.js';
 import fetch from 'node-fetch'
 
-const { proto } = (await import('@whiskeysockets/baileys')).default
-const isNumber = x => typeof x === 'number' && !isNaN(x)
-const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
-    clearTimeout(this)
-    resolve()
-}, ms))
-
-export async function handler(chatUpdate) {
-    this.msgqueque = this.msgqueque || []
-    this.uptime = this.uptime || Date.now()
-    if (!chatUpdate)
-        return
-    this.pushMessage(chatUpdate.messages).catch(console.error)
-    let m = chatUpdate.messages[chatUpdate.messages.length - 1]
-    if (!m)
-        return
-
-    if (global.db.data == null)
-        await global.loadDatabase()       
-    try {
-        m = smsg(this, m) || m
-        if (!m)
-            return
-
         const chatDB = global.db.data.chats[m.chat];
         if (chatDB && chatDB.botPrimario) {
             const universalWords = ['resetbot', 'resetprimario', 'botreset'];
@@ -44,56 +20,143 @@ export async function handler(chatUpdate) {
             }
         }
 
-        m.exp = 0
-        m.coin = false
-        try {
-            let user = global.db.data.users[m.sender]
-            if (typeof user !== 'object')
-                global.db.data.users[m.sender] = {}
-            if (user) {
-                if (!isNumber(user.exp)) user.exp = 0
-                if (!isNumber(user.coin)) user.coin = 10
-                if (!isNumber(user.joincount)) user.joincount = 1
-                if (!isNumber(user.diamond)) user.diamond = 3
-                if (!isNumber(user.lastadventure)) user.lastadventure = 0
-                if (!isNumber(user.lastclaim)) user.lastclaim = 0
-                if (!isNumber(user.health)) user.health = 100
-                if (!isNumber(user.crime)) user.crime = 0
-                if (!isNumber(user.lastcofre)) user.lastcofre = 0
-                if (!isNumber(user.lastdiamantes)) user.lastdiamantes = 0
-                if (!isNumber(user.lastpago)) user.lastpago = 0
-                if (!isNumber(user.lastcode)) user.lastcode = 0
-                if (!isNumber(user.lastcodereg)) user.lastcodereg = 0
-                if (!isNumber(user.lastduel)) user.lastduel = 0
-                if (!isNumber(user.lastmining)) user.lastmining = 0
-                if (!('muto' in user)) user.muto = false
-                if (!('premium' in user)) user.premium = false
-                if (!user.premium) user.premiumTime = 0
-                if (!('registered' in user)) user.registered = false
-                if (!('genre' in user)) user.genre = ''
-                if (!('birth' in user)) user.birth = ''
-                if (!('marry' in user)) user.marry = ''
-                if (!('description' in user)) user.description = ''
-                if (!('packstickers' in user)) user.packstickers = null
-                if (!user.registered) {
-                    if (!('name' in user)) user.name = m.name
-                    if (!isNumber(user.age)) user.age = -1
-                    if (!isNumber(user.regTime)) user.regTime = -1
-                }
-                if (!isNumber(user.afk)) user.afk = -1
-                if (!('afkReason' in user)) user.afkReason = ''
-                if (!('role' in user)) user.role = 'Nuv'
-                if (!('banned' in user)) user.banned = false
-                if (!('useDocument' in user)) user.useDocument = false
-                if (!isNumber(user.level)) user.level = 0
-                if (!isNumber(user.bank)) user.bank = 0
-                if (!isNumber(user.warn)) user.warn = 0
-            } else
-                global.db.data.users[m.sender] = {
-                    exp: 0, coin: 10, joincount: 1, diamond: 3, lastadventure: 0, health: 100, lastclaim: 0, lastcofre: 0, lastdiamantes: 0, lastcode: 0, lastduel: 0, lastpago: 0, lastmining: 0, lastcodereg: 0,
-                    muto: false, registered: false, genre: '', birth: '', marry: '', description: '', packstickers: null, name: m.name, age: -1, regTime: -1, afk: -1, afkReason: '', banned: false, useDocument: false,
-                    bank: 0, level: 0, role: 'Nuv', premium: false, premiumTime: 0,                 
-                }
+const { proto } = (await import('@whiskeysockets/baileys')).default
+const isNumber = x => typeof x === 'number' && !isNaN(x)
+const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
+    clearTimeout(this)
+    resolve()
+}, ms))
+
+ export async function handler(chatUpdate) {
+this.msgqueque = this.msgqueque || []
+this.uptime = this.uptime || Date.now()
+if (!chatUpdate)
+return
+this.pushMessage(chatUpdate.messages).catch(console.error)
+let m = chatUpdate.messages[chatUpdate.messages.length - 1]
+if (!m)
+return;
+if (global.db.data == null)
+await global.loadDatabase()       
+try {
+m = smsg(this, m) || m
+if (!m)
+return
+m.exp = 0
+m.coin = false
+try {
+let user = global.db.data.users[m.sender]
+if (typeof user !== 'object')  
+global.db.data.users[m.sender] = {}
+if (user) {
+if (!isNumber(user.exp))
+user.exp = 0
+if (!isNumber(user.coin))
+user.coin = 10
+if (!isNumber(user.joincount))
+user.joincount = 1
+if (!isNumber(user.diamond))
+user.diamond = 3
+if (!isNumber(user.lastadventure))
+user.lastadventure = 0
+if (!isNumber(user.lastclaim))
+user.lastclaim = 0
+if (!isNumber(user.health))
+user.health = 100
+if (!isNumber(user.crime))
+user.crime = 0
+if (!isNumber(user.lastcofre))
+user.lastcofre = 0
+if (!isNumber(user.lastdiamantes))
+user.lastdiamantes = 0
+if (!isNumber(user.lastpago))
+user.lastpago = 0
+if (!isNumber(user.lastcode))
+user.lastcode = 0
+if (!isNumber(user.lastcodereg))
+user.lastcodereg = 0
+if (!isNumber(user.lastduel))
+user.lastduel = 0
+if (!isNumber(user.lastmining))
+user.lastmining = 0
+if (!('muto' in user))
+user.muto = false
+if (!('premium' in user))
+user.premium = false
+if (!user.premium)
+user.premiumTime = 0
+if (!('registered' in user))
+user.registered = false
+if (!('genre' in user))
+user.genre = ''
+if (!('birth' in user))
+user.birth = ''
+if (!('marry' in user))
+user.marry = ''
+if (!('description' in user))
+user.description = ''
+if (!('packstickers' in user))
+user.packstickers = null
+if (!user.registered) {
+if (!('name' in user))
+user.name = m.name
+if (!isNumber(user.age))
+user.age = -1
+if (!isNumber(user.regTime))
+user.regTime = -1
+}
+if (!isNumber(user.afk))
+user.afk = -1
+if (!('afkReason' in user))
+user.afkReason = ''
+if (!('role' in user))
+user.role = 'Nuv'
+if (!('banned' in user))
+user.banned = false
+if (!('useDocument' in user))
+user.useDocument = false
+if (!isNumber(user.level))
+user.level = 0
+if (!isNumber(user.bank))
+user.bank = 0
+if (!isNumber(user.warn))
+user.warn = 0
+} else
+global.db.data.users[m.sender] = {
+exp: 0,
+coin: 10,
+joincount: 1,
+diamond: 3,
+lastadventure: 0,
+health: 100,
+lastclaim: 0,
+lastcofre: 0,
+lastdiamantes: 0,
+lastcode: 0,
+lastduel: 0,
+lastpago: 0,
+lastmining: 0,
+lastcodereg: 0,
+muto: false,
+registered: false,
+genre: '',
+birth: '',
+marry: '',
+description: '',
+packstickers: null,
+name: m.name,
+age: -1,
+regTime: -1,
+afk: -1,
+afkReason: '',
+banned: false,
+useDocument: false,
+bank: 0,
+level: 0,
+role: 'Nuv',
+premium: false,
+premiumTime: 0,                 
+}
             let chat = global.db.data.chats[m.chat]
             if (typeof chat !== 'object')
                 global.db.data.chats[m.chat] = {}
