3d4c85d71fd99d55521571eb1d4219199cbd3b2a
Dioneibi-rip <fandubdreams@gmail.com>
2025-10-09 18:50:52 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -6,6 +6,7 @@ import path, { join } from 'path'
 import { unwatchFile, watchFile } from 'fs'
 import chalk from 'chalk'
 import fetch from 'node-fetch'
+import failureHandler from './lib/respuesta.js';
 
 const { proto } = (await import('@whiskeysockets/baileys')).default
 const isNumber = x => typeof x === 'number' && !isNaN(x)
