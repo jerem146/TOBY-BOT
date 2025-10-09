7db7395b0f55565f88e5d68d7c4fd76985f48cc3
Dioneibi-rip <fandubdreams@gmail.com>
2025-10-09 19:48:43 -0400

    Actualizar rpg-transferir.js

--- plugins/rpg-transferir.js
+++ plugins/rpg-transferir.js
@@ -6,32 +6,39 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         who = m.chat
     }
 
-    if (!who) return m.reply(` etiquete al usuario a quien desea transferir. Ejemplo: *${usedPrefix + command} @usuario 100*`)
+    if (!who) return m.reply(`Etiquete al usuario a quien desea transferir.\n\n*Ejemplo:* ${usedPrefix + command} @usuario 100`)
     
+    if (!global.db.data.users[who]) {
+        return m.reply(`El usuario no está registrado en la base de datos.`);
+    }
+
     let amount = parseInt(args[1])
-    if (!amount || isNaN(amount) || amount <= 0) return m.reply(` ingrese una cantidad válida y mayor que cero. Ejemplo: *${usedPrefix + command} @usuario 100*`)
+    if (!amount || isNaN(amount) || amount <= 0) return m.reply(`Ingrese una cantidad válida y mayor que cero.\n\n*Ejemplo:* ${usedPrefix + command} @usuario 100`)
 
-    if (who === m.sender) return m.reply(' no puedes transferirte monedas a ti mismo.')
+    if (who === m.sender) return m.reply('No puedes transferirte monedas a ti mismo.')
 
-    let user = global.db.data.users[m.sender]
-    let recipient = global.db.data.users[who]
+    
+    let senderBank = global.db.data.users[m.sender].bank
+    if (senderBank < amount) return m.reply(`No tienes suficientes monedas en el banco para realizar esta transferencia.`)
 
-    if (typeof recipient.bank !== 'number') recipient.bank = 0
+    if (typeof global.db.data.users[who].bank !== 'number') {
+        global.db.data.users[who].bank = 0
+    }
 
-    if (user.bank < amount) return m.reply(` no tienes suficientes monedas en el banco para realizar esta transferencia.`)
-
-    user.bank -= amount
-    recipient.bank += amount
+    global.db.data.users[m.sender].bank -= amount
+    global.db.data.users[who].bank += amount
 
     let name = await conn.getName(who)
     let currency = '💰 Monedas'
-    m.reply(`✅ Transferiste exitosamente *${amount.toLocaleString()} ${currency}* a *${name}*.\n\n> Tu nuevo saldo en el banco es de *${user.bank.toLocaleString()} ${currency}*.`, null, { mentions: [who] })
+    let newSenderBalance = global.db.data.users[m.sender].bank
+
+    m.reply(`✅ Transferiste exitosamente *${amount.toLocaleString()} ${currency}* a *${name}*.\n\n> Tu nuevo saldo en el banco es de *${newSenderBalance.toLocaleString()} ${currency}*.`, null, { mentions: [who] })
 }
 
 handler.help = ['pay @usuario [cantidad]']
 handler.tags = ['rpg']
 handler.command = ['pay', 'transferir', 'darcoins']
 handler.group = true
-handler.register = true
+handler.register = true 
 
 export default handler
