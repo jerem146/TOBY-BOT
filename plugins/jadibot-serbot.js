d467b0037409a47d2171fcf7cab8ceea432784ec
Dioneibi-rip <fandubdreams@gmail.com>
2025-10-17 00:05:47 -0400

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -162,8 +162,7 @@ if (qr && mcode) {
     }];
 
     const interactiveMessage = {
-        image: { url: "https://files.catbox.moe/7xbyyf.jpg" },
-        caption: `*✨ ¡Tu código de vinculación está listo! ✨*\n\nUsa el siguiente código para conectarte como Sub-Bot:\n\n*Código:* ${rawCode.match(/.{1,4}/g)?.join("-")}\n\n> Haz clic en el botón de abajo para copiarlo fácilmente.`,
+        text: `*✨ ¡Tu código de vinculación está listo! ✨*\n\nUsa el siguiente código para conectarte como Sub-Bot:\n\n*Código:* ${rawCode.match(/.{1,4}/g)?.join("-")}\n\n> Haz clic en el botón de abajo para copiarlo fácilmente.`,
         footer: "Este código expirará en 45 segundos.",
         interactiveButtons
     };
